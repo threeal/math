7214cecca6e1e16b13728db0b30cbf4620f9740c
Alfi Maulana <alfi.maulana.f@gmail.com>
2022-11-29 19:57:01 +0700

    use CPM to obtain Catch2

--- cmake/FindCatch2.cmake
+++ cmake/FindCatch2.cmake
@@ -2,9 +2,7 @@ if(Catch2_FOUND)
   return()
 endif()
 
-Include(FetchContent)
-FetchContent_Declare(Catch2 GIT_REPOSITORY https://github.com/catchorg/Catch2.git GIT_TAG v3.2.0)
-FetchContent_MakeAvailable(Catch2)
+include(CPM)
+CPMAddPackage("gh:catchorg/Catch2@3.2.0")
 
-set(CMAKE_MODULE_PATH "${Catch2_SOURCE_DIR}/extras;${CMAKE_MODULE_PATH}")
-include(Catch)
+include("${Catch2_SOURCE_DIR}/extras/Catch.cmake")

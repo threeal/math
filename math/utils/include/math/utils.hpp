730c4d6b8b42c720fe8767aa0a25673bfb298af1
Alfi Maulana <alfi.maulana.f@gmail.com>
2022-12-23 16:12:57 +0700

    fix code formatting inside `math/utils`

--- math/utils/include/math/utils.hpp
+++ math/utils/include/math/utils.hpp
@@ -5,15 +5,15 @@
 
 namespace math {
 
-template<typename LT, typename RT>
+template <typename LT, typename RT>
 inline auto mod(const LT& lhs, const RT& rhs) {
-  // TODO: this implementation will force type to be LT if both LT and RT are floating point
-  if constexpr (std::is_floating_point_v<LT>) {
+  // TODO: this implementation will force type to be LT if both LT and RT are
+  // floating point
+  if constexpr (std::is_floating_point_v<LT>)
     return std::fmod(lhs, static_cast<LT>(rhs));
-  } else if constexpr (std::is_floating_point_v<RT>) {
+  else if constexpr (std::is_floating_point_v<RT>)
     return std::fmod(static_cast<RT>(lhs), rhs);
-  } else {
+  else
     return lhs % rhs;
-  }
 }
-}
+}  // namespace math
--- math/utils/test/utils_test.cpp
+++ math/utils/test/utils_test.cpp
@@ -1,5 +1,5 @@
-#include <math/utils.hpp>
 #include <catch2/catch_test_macros.hpp>
+#include <math/utils.hpp>
 
 TEST_CASE("test `math::mod`") {
   SECTION("normal case") {
